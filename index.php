2bc34751f19a585e9b05859d1b6df0682bb18847
morcsog <morcsog99@gmail.com>
2024-10-24 15:48:06 +0200

    Creates a test.env file
    Creates a test.env file, var_dumps the $_ENV superglobal out and sets the requirements for dotenv.

--- index.php
+++ index.php
@@ -2,3 +2,14 @@
 
 declare(strict_types=1);
 //--sort-packages option keeps the package list sorted. Use this with the composer require command.
+
+require __DIR__ . '/vendor/autoload.php';
+
+use Symfony\Component\Dotenv\Dotenv;
+
+$dotenv = new Dotenv();
+$dotenv->load(__DIR__ . '/test.env');
+
+echo '<pre>';
+var_dump($_ENV);
+echo '</pre>';
